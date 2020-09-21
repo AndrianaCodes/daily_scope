aa35d35ab28df1eb7286011bcf3347aed661ac68
AndrianaCodes <andriana.amg@gmail.com>
2020-09-20 20:23:43 -0400

    created horoscope file and deleted sign and date file

---
+++ lib/daily_scope/horoscope.rb
@@ -0,0 +1,19 @@
+class DailyScope::Horoscope 
+    
+    attr_accessor :name, :facts 
+    @@all = [] 
+    
+    def initialize(name) 
+        @name = name 
+        @facts = facts 
+        save 
+    end 
+    
+    def self.all 
+        @@all 
+    end 
+    
+    def save 
+        @@all << self 
+    end 
+end 
